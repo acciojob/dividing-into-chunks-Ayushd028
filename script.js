0888f0fa6cfe297d5ea91d2da664a795c9611b3d
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-02-24 15:39:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	
+  let result = [];
+  let subarray = [];
+  let sum = 0;
+
+  arr.forEach((num) => {
+    if (sum + num > n) {
+      result.push(subarray);
+      subarray = [];
+      sum = 0;
+    }
+    subarray.push(num);
+    sum += num;
+  });
+
+  if (subarray.length) result.push(subarray);
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
